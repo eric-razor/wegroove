b4c5e813279d444c4f124e2ae2d8e1109fe0cce0
eric <eric.razorjr@gmail.com>
2021-05-08 14:53:32 -0400

    API redirect

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,7 +5,11 @@ import useFetch from '../hooks/useFetch'
 export default function Login(){
 
     function redirect () {
-        window.location.assign("https://accounts.spotify.com/authorize/")
+        window.location.assign('https://accounts.spotify.com/authorize' +
+            '?response_type=code' +
+            '&client_id=' + `${process.env.REACT_APP_CLIENT_ID}` +
+            (`${process.env.REACT_APP_SCOPES}` ? '&scope=' + encodeURIComponent(`${process.env.REACT_APP_SCOPES}`) : '') +
+            '&redirect_uri=' + encodeURIComponent(`${process.env.REACT_APP_REDIRECT_URI}`))
     }
     return (
         <div>
