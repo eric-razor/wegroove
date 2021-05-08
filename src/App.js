a0c9632b41a139e726eef7e7af7a1f1a90640714
eric <eric.razorjr@gmail.com>
2021-05-08 14:54:17 -0400

    Add env var

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import Login from './components/Login'
 
 
-const { REACT_APP_CLIENT_ID, REACT_APP_REDIRECT_URI} = process.env
+const { REACT_APP_CLIENT_ID, REACT_APP_REDIRECT_URI, REACT_APP_SCOPES} = process.env
 
 function App() {
   return (
