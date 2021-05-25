c46b54d69e524482d41ada68c0d6d895c54e0f51
eric <eric.razorjr@gmail.com>
2021-05-25 19:11:18 -0400

    Add router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-
+import React from 'react'
 import Login from './components/Login'
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 const { REACT_APP_CLIENT_ID, REACT_APP_REDIRECT_URI, REACT_APP_SCOPES} = process.env
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <Login/>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
