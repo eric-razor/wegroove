646e6415819eacdb4a6202f708c051f834fc6562
eric <eric.razorjr@gmail.com>
2021-05-26 18:36:26 -0400

    Refactor AJAX flow

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,32 +1,41 @@
 import React from 'react';
-import useFetch from '../hooks/useFetch'
+export {onPageLoad,}
 
 
-export default function Login(){
+function redirect() {
+    let url = 'https://accounts.spotify.com/authorize' +
+        '?response_type=code' +
+        '&client_id=' + `${process.env.REACT_APP_CLIENT_ID}` +
+        (`${process.env.REACT_APP_SCOPES}` ? '&scope=' + encodeURIComponent(`${process.env.REACT_APP_SCOPES}`) : '') +
+        '&redirect_uri=' + encodeURIComponent(`${process.env.REACT_APP_REDIRECT_URI}`)
 
-    function redirect() {
-        window.location.assign('https://accounts.spotify.com/authorize' +
-            '?response_type=code' +
-            '&client_id=' + `${process.env.REACT_APP_CLIENT_ID}` +
-            (`${process.env.REACT_APP_SCOPES}` ? '&scope=' + encodeURIComponent(`${process.env.REACT_APP_SCOPES}`) : '') +
-            '&redirect_uri=' + encodeURIComponent(`${process.env.REACT_APP_REDIRECT_URI}`))
+    window.location.href = url
+}
+
+function getCodeParam() {
+    let code = null
+    const queryString = window.location.search;
+    if (queryString.length > 0 ){
+        const urlParams = new URLSearchParams(queryString)
+        code = urlParams.get('code');
     }
+    return code
+}
 
-    function getCodeParam() {
-        let params = (new URL(document.location)).searchParams;
-        let code = params.get('code');
-        return code
-    }
+function handleRedirect(){
+    let code = getCodeParam() 
+    fetchAccessToken(code)
+}
 
-    function requestTokens() {
-        let token = getCodeParam()
-        fetch("/api/v1/token",{
-            method: 'POST',
-            headers: {
-                'Content-Type' : 'application/json'
-            },
-            body: JSON.stringify({"code": token, "redirect_uri": `${process.env.REACT_APP_REDIRECT_URI}`})      
-        })
+function fetchAccessToken(code){
+    let code_params = { 'code': code, 'redirect_uri': `${process.env.REACT_APP_REDIRECT_URI}` }
+    fetch("/api/v1/token", {
+        method:'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(code_params)
+    })
         .then(resp => resp.json())
         .then(data => {
             console.log('Success: ', data)
@@ -34,6 +43,18 @@ export default function Login(){
         .catch((error) => {
             console.error('Error: ', error)
         })
+}
+// named export {}
+function onPageLoad() {
+    if (window.location.search.length > 0) {
+        handleRedirect()
+    }
+}
+
+export default function Login(){
+  
+    function requestTokens() {
+        
 
     }
 
