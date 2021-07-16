a6a7ad66dec00053486b6b3f19ab4f6e61834395
eric <eric.razorjr@gmail.com>
2021-07-16 14:27:58 -0400

    Return foreign URI

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,78 +1,33 @@
 import React from 'react';
-import { getUserProfile } from './Profile';
-export {onPageLoad, redirect}
 
-function redirect() {
-    // accept scopes
+function getRedirectUrl() {
     let url = 'https://accounts.spotify.com/authorize' +
         '?response_type=code' +
-        '&client_id=' + `${process.env.REACT_APP_CLIENT_ID}` +
-        (`${process.env.REACT_APP_SCOPES}` ? '&scope=' + encodeURIComponent(`${process.env.REACT_APP_SCOPES}`) : '') +
-        '&redirect_uri=' + encodeURIComponent(`${process.env.REACT_APP_REDIRECT_URI}`)
+        '&client_id=' + 
+        `${process.env.REACT_APP_CLIENT_ID}` +
+        (`${process.env.REACT_APP_SCOPES}` ? 
+        '&scope=' + 
+        encodeURIComponent(`${process.env.REACT_APP_SCOPES}`) : '') +
+        '&redirect_uri=' + 
+        encodeURIComponent(`${process.env.REACT_APP_REDIRECT_URI}`)
 
-    window.location.href = url
+    return url;
 }
 
-function getCodeParam() {
-    let code = null
-    const queryString = window.location.search;
-    if (queryString.length > 0 ){
-        const urlParams = new URLSearchParams(queryString)
-        code = urlParams.get('code');
-    }
-    return code
-}
-
-function handleRedirect(){
-    let code = getCodeParam() 
-    fetchAccessToken(code)
-    // remove code param from URL
+function clearCodeQuery(){
     window.history.pushState("", "", process.env.REACT_APP_REDIRECT_URI)
 }
 
-function fetchAccessToken(code){
-    let code_params = { 'code': code, 'redirect_uri': `${process.env.REACT_APP_REDIRECT_URI}` }
-
-    fetch("/api/v1/token", {
-        method:'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(code_params)
-    })
-    .then(resp => resp.json())
-    .then(data => {
-        handleAuthTokens(data)
-    })
-    .catch((error) => {
-        console.error('Error: ', error)
-    })
-}
-
-function handleAuthTokens(code){
-    if(code.access_token !== undefined || code.refresh_token !== undefined){
-        let access_token = code.access_token
-        let refresh_token = code.refresh_token
-        localStorage.setItem("access_token", access_token)
-        localStorage.setItem("refresh_token", refresh_token)
-    } else {
-        // 
-    }
-}
-
-
-// exports
 function onPageLoad() {
     if (window.location.search.length > 0) {
-        handleRedirect()
+        clearCodeQuery()
     }
 }
 
 function Login(){
+    onPageLoad();
     return (
-        <div>
-            <button>login</button>
-        </div>
+        <a className="nav-route-link" href={getRedirectUrl()}>Login</a>
     )
 }
 
